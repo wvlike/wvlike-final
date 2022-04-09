5e2572c6e8748b35c51efe20a29502c1ce50b4aa
wvlike <1515086122@qq.com>
2022-04-09 17:30:18 +0800

    init

---
+++ wvlike-gateway/src/main/java/com/wvlike/gateway/GatewayApp.java
@@ -0,0 +1,38 @@
+package com.wvlike.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * package com.wvlike.gateway;
+ *
+ * @auther txw
+ * @create 2021-02-17  9:43
+ * @description：
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class);
+    }
+
+
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                String hostName = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+                System.out.println("hostName:" + hostName);
+                return Mono.just(hostName);
+            }
+        };
+    }
+}
---
+++ wvlike-gateway/src/main/java/com/wvlike/gateway/controller/FallbackController.java
@@ -0,0 +1,24 @@
+package com.wvlike.gateway.controller;
+
+import com.wvlike.gateway.common.ResultDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * package com.wvlike.gateway.controller;
+ *
+ * @auther txw
+ * @create 2021-02-17  16:13
+ * @description：
+ */
+@Slf4j
+@RestController
+public class FallbackController {
+
+    @PostMapping("/fallback")
+    public ResultDTO fallback() {
+        return ResultDTO.fail("网关错误");
+    }
+
+}
