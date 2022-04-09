2755f40f3bb9ecb005f075b54b104aefc3bbe14d
wvlike <1515086122@qq.com>
2022-04-09 17:19:38 +0800

    init

---
+++ wvlike-eureka-server/src/main/java/com/ismyself/eureka/server/EurekaDemoApp.java
@@ -0,0 +1,21 @@
+package com.ismyself.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * package com.ismyself.eurekaDemo;
+ *
+ * @auther txw
+ * @create 2020-02-10  10:10
+ * @description：
+ */
+@SpringBootApplication
+@EnableEurekaServer//开启注册中心服务
+public class EurekaDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemoApp.class);
+    }
+}
