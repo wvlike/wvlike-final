b55928ea347a6bc40bd4694d8ef23bf8a6eed933
xinwen_tu <xinwen_tu@kingdee.com>
2023-03-08 17:33:50 +0800

    wenti

--- wvlike-service/wvlike-user-service/src/main/java/com/wvlike/user/archaius/ArchaiusTest.java
+++ wvlike-service/wvlike-user-service/src/main/java/com/wvlike/user/archaius/ArchaiusTest.java
@@ -2,6 +2,7 @@ package com.wvlike.user.archaius;
 
 import com.netflix.config.DynamicPropertyFactory;
 import com.netflix.config.DynamicStringProperty;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,12 +14,21 @@ import java.util.concurrent.TimeUnit;
 public class ArchaiusTest {
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
+        test02();
+    }
+
+    private static void test02() throws Exception {
+        PropertiesConfiguration propertiesConfiguration = new PropertiesConfiguration("app.properties");
+        System.out.println(propertiesConfiguration.getString("app.test"));
+    }
+
+    private static void test01() throws InterruptedException {
         DynamicPropertyFactory instance = DynamicPropertyFactory.getInstance();
         DynamicStringProperty stringProperty = instance.getStringProperty("con1", "defalut");
         stringProperty.addCallback(() -> System.out.println("con1 属性值发生变化！"));
-        while (true){
+        while (true) {
             System.out.println(stringProperty.get());
             TimeUnit.SECONDS.sleep(10);
         }
