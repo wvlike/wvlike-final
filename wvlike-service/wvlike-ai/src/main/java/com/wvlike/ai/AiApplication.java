678c9aed97c41375b1305d5675b4f88399ad2d19
xinwen_tu <1515086122@qq.com>
2026-02-03 14:58:33 +0800

    AI项目

---
+++ wvlike-service/wvlike-ai/src/main/java/com/wvlike/ai/AiApplication.java
@@ -0,0 +1,16 @@
+package com.wvlike.ai;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author: xinwen_tu
+ * @Date: 2026/2/3
+ * Description:
+ */
+public class AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiApplication.class);
+    }
+
+}
